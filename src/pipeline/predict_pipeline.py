916dcb02a30618087b0261e4ec1a5025185a7cbd
sanjay231192 <sanjaysanalkm@gmail.com>
2024-06-01 21:32:27 +0200

    update

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -11,14 +11,14 @@ class PredictPipeline:
 
     def predict(self,features):
         try:
-            #model_path=os.path.join("artifacts","model.pkl")
-            model_path = r'artifacts\model.pkl'
-            #preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
-            preprocessor_path = r'artifacts\preprocessor.pkl'
-            #print("Before Loading")
+            model_path=os.path.join("artifacts","model.pkl")
+            #model_path = r'artifacts\model.pkl'
+            preprocessor_path=os.path.join('artifacts','preprocessor.pkl')
+            #preprocessor_path = r'artifacts\preprocessor.pkl'
+            print("Before Loading")
             model=load_object(file_path=model_path)
             preprocessor=load_object(file_path=preprocessor_path)
-            #print("After Loading")
+            print("After Loading")
             data_scaled=preprocessor.transform(features)
             preds=model.predict(data_scaled)
             return preds
